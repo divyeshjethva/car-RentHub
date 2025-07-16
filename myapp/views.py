afd9e21d21773fba55a651ae61a13ff04073b49d
divyeshjethva <divyeshjethva2004@gmail.com>
2025-07-16 14:37:26 +0530

    car rentHub

---
+++ myapp/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render,redirect
+from .models import *
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def register(request):
+    if request.method == "POST":
+        try:
+            user = User.objects.get(email=request.POST['email'])
+            if user:
+                msg = "Email is Already Exists"
+                return render(request,'register.html',{'msg':msg})
+        except:
+            if request.POST['password']==request.POST['cpassword']:
+                User.objects.create(
+                    name = request.POST['name'],
+                    email = request.POST['email'],
+                    mobile = request.POST['mobile'],
+                    password = request.POST['password'],
+                )
+                msg = "Signup successfull"
+                return render(request,'login.html',{'msg':msg})
+            else:
+                msg = "Passwor does not match"
+                return render(request,'register.html',{'msg':msg})
+    else:
+        return render(request,'register.html')
+
+def login(request):
+    if request.method == "POST":
+        try:
+            user = User.objects.get(email=request.POST['email'])
+            if user.email == request.POST['email']:
+                if user.password == request.POST['password']:
+                    return redirect('index')
+                else:
+                    msg = "Password does not match"
+                    return render(request,'login.html',{'msg':msg})
+            else:
+                msg = "Email is Not found"
+                return render(request,'login.html',{'msg':msg})
+        except:
+            msg = "User is Not found"
+            return render(request,'login.html',{'msg':msg})
+    else:
+        return render(request,'login.html')
+    
